92beaa98a8796869a79ae78f2764ebf25971a644
Luiz <luizeduardo2099@gmail.com>
2025-06-02 18:09:17 -0300

    bugFix

--- src/components/ExerciseItem.jsx
+++ src/components/ExerciseItem.jsx
@@ -6,23 +6,25 @@ import React from 'react';
 
 
 function ExerciseItem({ exercise, onEdit, onDelete }) {
-    return (
-      <li className='exercise-item'>
-        <div className='exercise-header'>
-          <strong className='exercise-title'>{exercise.name}</strong>
-          <div className='exercise-buttons'>
-            <button onClick={onEdit}>Editar</button>
-            <button onClick={onDelete}>Remover</button>
-          </div>
-        </div>
+  return (
+    <li className='exercise-item'>
+  <div className='exercise-header'>
+    <strong className='exercise-title'>{exercise.name}</strong>
+    <p className='exercise-type'>{exercise.type}</p>
+  </div>
+
+  <div className='exercise-stats'>
+    <div><strong>Peso:</strong> {exercise.weight}kg</div>
+    <div><strong>Repetições:</strong> {exercise.reps}</div>
+  </div>
+
+  <div className='exercise-buttons'>
+    <button onClick={onEdit}>Editar</button>
+    <button onClick={onDelete}>Remover</button>
+  </div>
+</li>
+
+  );
+}
   
-        <p className='exercise-type'>{exercise.type}</p>
-  
-        <div className='exercise-stats'>
-          <span><strong>Peso:</strong> {exercise.weight}kg</span>
-          <span><strong>Repetições:</strong> {exercise.reps}</span>
-        </div>
-      </li>
-    );
-  }
-  
+  export default ExerciseItem;
